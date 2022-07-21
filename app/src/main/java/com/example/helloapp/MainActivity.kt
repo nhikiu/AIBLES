178545bda5208912c6200a024789caf0449cd02a
Nhi Do <dongocnhi0110@gmail.com>
2022-07-22 04:58:46 +0700

    Update

--- app/src/main/java/com/example/helloapp/MainActivity.kt
+++ app/src/main/java/com/example/helloapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.helloapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.buttonSignIn)
+        button.setOnClickListener{
+            button.setBackgroundColor(Color.BLUE)
+        }
     }
 }
